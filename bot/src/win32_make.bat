c9fe45ded7695c2463301fd46cf7c971b95bb410
93aef0ce4dd141ece6f5 <asdfhui@gmail.com>
2016-01-28 19:17:22 +1100

    updated install.c

--- bot/src/win32_make.bat
+++ bot/src/win32_make.bat
@@ -1,4 +1,4 @@
 @echo off
-gcc -Wall -Werror -O -o bot bot.c init.c recv.c exec.c dos.c -lws2_32
+gcc -Wall -Werror -O -o ../win32exe/bot bot.c init.c recv.c exec.c dos.c install.c -lws2_32
 echo Done.
 pause >NUL
